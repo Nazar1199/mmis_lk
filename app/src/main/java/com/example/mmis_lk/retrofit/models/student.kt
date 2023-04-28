4a12e729ea16002daa37c4f400dcd3e7505be753
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-04-29 01:13:03 +0300

    feat: added student

---
+++ app/src/main/java/com/example/mmis_lk/retrofit/models/student.kt
@@ -0,0 +1,12 @@
+package com.example.mmis_lk.retrofit.models
+
+data class student(
+    val birthdate: String,
+    val email: String,
+    val firstName: String,
+    val id: Int,
+    val lastName: String,
+    val patronymic: String,
+    val phone: String,
+    val reportCard: Int
+)
