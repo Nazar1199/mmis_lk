e9f5b4dadeb5a6ac5ca579432c3c55ff7ff7fcf4
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-04-28 05:35:28 +0300

    fix: fixed server address

--- app/src/main/java/com/example/mmis_lk/MainActivity.kt
+++ app/src/main/java/com/example/mmis_lk/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import com.example.mmis_lk.retrofit.RetrofitClient
 import com.example.mmis_lk.retrofit.interfaces.mmis_api
 import kotlinx.coroutines.*
@@ -17,27 +16,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val localBaseUrl = "http://192.168.0.13:2000/api/"
+        val localBaseUrl = "http://192.168.0.12:2000/api/"
 
-        var loginEmail = findViewById<EditText>(R.id.editTextTextEmail)
-        var loginPassword = findViewById<EditText>(R.id.editTextTextPassword)
-        var messagesView = findViewById<TextView>(R.id.textViewMessage)
-        var loginBt = findViewById<Button>(R.id.buttonLogin)
+        val loginEmail = findViewById<EditText>(R.id.editTextTextEmail)
+        val loginPassword = findViewById<EditText>(R.id.editTextTextPassword)
+        val messagesView = findViewById<TextView>(R.id.textViewMessage)
+        val loginBt = findViewById<Button>(R.id.buttonLogin)
         loginBt.setOnClickListener {
-//            Toast.makeText(this, "Тостик для Дафки v.2", Toast.LENGTH_SHORT).show()
             val email = loginEmail.text.toString()
             val password = loginPassword.text.toString()
                 val client = RetrofitClient.getClient(localBaseUrl)
-                var api = client.create(mmis_api::class.java)
+                val api = client.create(mmis_api::class.java)
                 CoroutineScope(Dispatchers.IO).launch {
                     try {
-                        var log = UserLogin(email, password)
+                        val log = UserLogin(email, password)
                         val profile = api.login(log)
                         runOnUiThread {
                             if (profile.id != null){
                                 messagesView.text = "Вход успешный!"
                             }
-//                            messagesView.text = profile.toString()
                         }
                     } catch (error: Exception){
                         runOnUiThread {
