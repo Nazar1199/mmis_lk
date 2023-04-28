82ab3745597c4734bc7c57393ce8fba35185f0c4
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-04-29 01:11:08 +0300

    feat: added getMyProfile

--- app/src/main/java/com/example/mmis_lk/retrofit/interfaces/mmisApi.kt
+++ app/src/main/java/com/example/mmis_lk/retrofit/interfaces/mmisApi.kt
@@ -2,12 +2,16 @@ package com.example.mmis_lk.retrofit.interfaces
 
 import com.example.mmis_lk.retrofit.models.userLogin
 import com.example.mmis_lk.retrofit.models.profile
-import retrofit2.http.Body
-import retrofit2.http.POST
+import com.example.mmis_lk.retrofit.models.student
+import com.example.mmis_lk.retrofit.models.token
+import retrofit2.http.*
 
 interface mmisApi {
 //    @POST("profile/login")
 //    fun login(@Body email: String, password: String): profile
     @POST("profile/login")
-    suspend fun login(@Body user: userLogin): profile
+    suspend fun login(@Body user: profile): token
+
+    @GET("student/me")
+    suspend fun getMyProfile(@Header("Authorization") token: String): student
 }
