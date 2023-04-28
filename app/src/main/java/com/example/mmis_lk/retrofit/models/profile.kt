9dd0b24df96ae256567abaa7af99528792d888b2
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-04-28 21:04:24 +0300

    fix: profile changed

--- app/src/main/java/com/example/mmis_lk/retrofit/models/profile.kt
+++ app/src/main/java/com/example/mmis_lk/retrofit/models/profile.kt
@@ -1,9 +1,7 @@
 package com.example.mmis_lk.retrofit.models
 
 data class profile(
-    val firstName: String,
     val id: Int,
-    val lastName: String,
-    val login: String,
+    val email: String,
     val password: String
 )
