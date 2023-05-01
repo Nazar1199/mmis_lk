9a3bde21813f471bff37a5773b6cd01f2e33cea5
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-05-01 16:11:57 +0300

    fix: fixed date type

--- app/src/main/java/com/example/mmis_lk/retrofit/models/OrderingReference.kt
+++ app/src/main/java/com/example/mmis_lk/retrofit/models/OrderingReference.kt
@@ -1,8 +1,10 @@
 package com.example.mmis_lk.retrofit.models
 
+import java.util.Date
+
 data class OrderingReference(
-    val date: String,
     val id: Int,
+    val date: Date,
     val reference: Reference,
     val status: Status
 )
