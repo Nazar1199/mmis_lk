0cddbf19beac50bc8c3f8d2510a54adcd3d2840a
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-05-01 09:41:18 +0300

    fix: deleted not using code

--- app/src/main/java/com/example/mmis_lk/retrofit/interfaces/mmisApi.kt
+++ app/src/main/java/com/example/mmis_lk/retrofit/interfaces/mmisApi.kt
@@ -6,8 +6,6 @@ import com.example.mmis_lk.retrofit.models.Token
 import retrofit2.http.*
 
 interface mmisApi {
-//    @POST("profile/login")
-//    fun login(@Body email: String, password: String): profile
     @POST("profile/login")
     suspend fun login(@Body user: Profile): Token
 
