99888ccd4905856678e1000e05ad62fa9cd26dba
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-05-06 02:39:24 +0300

    feat: added Teacher model

---
+++ app/src/main/java/com/example/mmis_lk/retrofit/models/Teacher.kt
@@ -0,0 +1,12 @@
+package com.example.mmis_lk.retrofit.models
+
+data class Teacher(
+    val email: String,
+    val firstName: String,
+    val id: Int,
+    val lastName: String,
+    val position: Position,
+    val patronymic: String,
+    val phone: String,
+    val photo: String
+)
