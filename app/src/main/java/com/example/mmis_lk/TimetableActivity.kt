32ae761b4647be81ae4f5ba76fbab241c37e09a1
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-05-13 06:37:43 +0300

    fix: fixed working timetable

--- app/src/main/java/com/example/mmis_lk/TimetableActivity.kt
+++ app/src/main/java/com/example/mmis_lk/TimetableActivity.kt
@@ -4,10 +4,10 @@ import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mmis_lk.adapters.DayTimetableAdapter
-import com.example.mmis_lk.classes.ReferenceListDialog
 import com.example.mmis_lk.retrofit.RetrofitClient
 import com.example.mmis_lk.retrofit.interfaces.mmisApi
 import com.example.mmis_lk.retrofit.models.TimeTable
@@ -30,26 +30,41 @@ class TimetableActivity : AppCompatActivity() {
         val api = client.create(mmisApi::class.java)
         CoroutineScope(Dispatchers.IO).launch {
             try {
-                var timetables = api.getAllTimetables(savedToken.toString())
+                var timetables: Array<TimeTable> = arrayOf()
+                if (intent.getBooleanExtra("filterByGroup", false)) {
+                    val groupId = intent.getIntExtra("currentGroupIdFilter", 0)
+                    timetables = api.getTimetableByGroup(savedToken.toString(), groupId)
+                } else
+                if (intent.getBooleanExtra("filterByTeacher", false)) {
+                    val teacherId = intent.getIntExtra("currentTeacherIdFilter", 0)
+                    timetables = api.getTimetableByTeacher(savedToken.toString(), teacherId)
+                } else
+                if (intent.getBooleanExtra("filterByAuditorium", false)) {
+                    val auditoriumId = intent.getIntExtra("currentAuditoriumIdFilter", 0)
+                    timetables = api.getTimetableByAuditorium(savedToken.toString(), auditoriumId)
+                }
+
                 timetables.sortBy { it.date }
                 var sortingTimetables: MutableList<Array<TimeTable>> = arrayListOf()
                 var sortingTimetablesForDay: MutableList<TimeTable> = arrayListOf()
-                var startDate = timetables[0].date
-                timetables.forEach {
-                    if (it.date == startDate){
-                        sortingTimetablesForDay.add(it)
-                    } else {
-                        sortingTimetables.add(sortingTimetablesForDay.toTypedArray())
-                        sortingTimetablesForDay = arrayListOf()
-                        sortingTimetablesForDay.add(it)
-                        startDate = it.date
+                if (timetables.isNotEmpty()){
+                    var startDate = timetables[0].date
+                    timetables.forEach {
+                        if (it.date == startDate){
+                            sortingTimetablesForDay.add(it)
+                        } else {
+                            sortingTimetables.add(sortingTimetablesForDay.toTypedArray())
+                            sortingTimetablesForDay = arrayListOf()
+                            sortingTimetablesForDay.add(it)
+                            startDate = it.date
+                        }
                     }
+                    sortingTimetables.add(sortingTimetablesForDay.toTypedArray())
+                    val arr: Array<Array<TimeTable>> = sortingTimetables.toTypedArray()
+                    runOnUiThread { recyclerList.adapter = DayTimetableAdapter(arr) }
                 }
-                sortingTimetables.add(sortingTimetablesForDay.toTypedArray())
-                val arr: Array<Array<TimeTable>> = sortingTimetables.toTypedArray()
-                recyclerList.adapter = DayTimetableAdapter(arr)
             } catch (error: Exception){
-                
+
             }
         }
     }
